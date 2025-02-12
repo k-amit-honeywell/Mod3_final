104de61e772afd51f4aea5dcb64e10c155645139
Amit-Kulkarni_HON <amit.kulkarni@honeywell.com>
2025-02-12 09:07:35 -0500

    Initial Commit

---
+++ emotion_detection.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+def emotion_predictor(text_to_analyse):
+    # URL of the sentiment analysis service
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+
+    # Constructing the request payload in the expected format
+    myobj = { "raw_document": { "text": text_to_analyse }, "document_emotion": True}
+
+    # Custom header specifying the model ID for the emotion detector service
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+    # Sending a POST request to the emotion analysis API
+    response = requests.post(url, json=myobj, headers=header)
+
+    # Parsing the JSON response from the API
+    formatted_response = json.loads(response.text)
+
+    # Extracting emotion from the response
+    emotion = formatted_response['emotionPredictions']
+
+    # Returning a dictionary containing emotion analysis results
+    return {'emotion': emotion}
